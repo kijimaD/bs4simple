3cfeb30340c88b5cf83595c8979c0f2bc8656632
kijima <norimaking777@gmail.com>
2020-05-29 17:22:07 +0900

    Fix padding

--- main.php
+++ main.php
@@ -15,80 +15,80 @@ $hasSidebar = page_findnearest($conf['sidebar']);
 $showSidebar = $hasSidebar && ($ACT=='show');
 ?><!DOCTYPE html>
 <html lang="<?php echo $conf['lang'] ?>" dir="<?php echo $lang['direction'] ?>" class="no-js">
-<head>
-    <meta charset="utf-8" />
-    <title><?php tpl_pagetitle() ?> [<?php echo strip_tags($conf['title']) ?>]</title>
-    <script>(function(H){H.className=H.className.replace(/\bno-js\b/,'js')})(document.documentElement)</script>
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" />
-    <?php tpl_metaheaders() ?>
-    <meta name="viewport" content="width=device-width,initial-scale=1" />
-    <?php echo tpl_favicon(array('favicon', 'mobile')) ?>
-    <?php tpl_includeFile('meta.html') ?>
-</head>
+    <head>
+	<meta charset="utf-8" />
+	<title><?php tpl_pagetitle() ?> [<?php echo strip_tags($conf['title']) ?>]</title>
+	<script>(function(H){H.className=H.className.replace(/\bno-js\b/,'js')})(document.documentElement)</script>
+	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" />
+	<?php tpl_metaheaders() ?>
+	<meta name="viewport" content="width=device-width,initial-scale=1" />
+	<?php echo tpl_favicon(array('favicon', 'mobile')) ?>
+	<?php tpl_includeFile('meta.html') ?>
+    </head>
 
-<body>
-    <div id="dokuwiki__site"><div id="dokuwiki__top" class="site <?php echo tpl_classes(); ?> <?php
-        echo ($showSidebar) ? 'showSidebar' : ''; ?> <?php echo ($hasSidebar) ? 'hasSidebar' : ''; ?>">
+    <body>
+	<div id="dokuwiki__site"><div id="dokuwiki__top" class="site <?php echo tpl_classes(); ?> <?php
+												  echo ($showSidebar) ? 'showSidebar' : ''; ?> <?php echo ($hasSidebar) ? 'hasSidebar' : ''; ?>">
 
-        <?php include('tpl_header.php') ?>
+            <?php include('tpl_header.php') ?>
+	    <div class="container">
+		<div class="row">
 
-        <div class="wrapper group">
+			<?php if($showSidebar): ?>
+			    <!-- ********** ASIDE ********** -->
+			    <div id="dokuwiki__aside"><div class="pad aside include group">
+				<h3 class="toggle"><?php echo $lang['sidebar'] ?></h3>
+				<div class="content"><div class="group">
+				    <?php tpl_flush() ?>
+				    <?php tpl_includeFile('sidebarheader.html') ?>
+				    <?php tpl_include_page($conf['sidebar'], true, true) ?>
+				    <?php tpl_includeFile('sidebarfooter.html') ?>
+				</div></div>
+			    </div></div><!-- /aside -->
+			<?php endif; ?>
 
-            <?php if($showSidebar): ?>
-                <!-- ********** ASIDE ********** -->
-                <div id="dokuwiki__aside"><div class="pad aside include group">
-                    <h3 class="toggle"><?php echo $lang['sidebar'] ?></h3>
-                    <div class="content"><div class="group">
-                        <?php tpl_flush() ?>
-                        <?php tpl_includeFile('sidebarheader.html') ?>
-                        <?php tpl_include_page($conf['sidebar'], true, true) ?>
-                        <?php tpl_includeFile('sidebarfooter.html') ?>
-                    </div></div>
-                </div></div><!-- /aside -->
-            <?php endif; ?>
+			<!-- ********** CONTENT ********** -->
+			<div id="dokuwiki__content"><div class="pad group">
+			    <?php html_msgarea() ?>
 
-            <!-- ********** CONTENT ********** -->
-            <div id="dokuwiki__content"><div class="pad group">
-                <?php html_msgarea() ?>
+			    <div class="pageId"><span><?php echo hsc($ID) ?></span></div>
 
-                <div class="pageId"><span><?php echo hsc($ID) ?></span></div>
+			    <div class="page group">
+				<?php tpl_flush() ?>
+				<?php tpl_includeFile('pageheader.html') ?>
+				<!-- wikipage start -->
+				<?php tpl_content() ?>
+				<!-- wikipage stop -->
+				<?php tpl_includeFile('pagefooter.html') ?>
+			    </div>
 
-                <div class="page group">
-                    <?php tpl_flush() ?>
-                    <?php tpl_includeFile('pageheader.html') ?>
-                    <!-- wikipage start -->
-                    <?php tpl_content() ?>
-                    <!-- wikipage stop -->
-                    <?php tpl_includeFile('pagefooter.html') ?>
-                </div>
+			    <div class="docInfo"><?php tpl_pageinfo() ?></div>
 
-                <div class="docInfo"><?php tpl_pageinfo() ?></div>
+			    <?php tpl_flush() ?>
+			</div></div><!-- /content -->
 
-                <?php tpl_flush() ?>
-            </div></div><!-- /content -->
+			<hr class="a11y" />
 
-            <hr class="a11y" />
+			<!-- PAGE ACTIONS -->
+			<div id="dokuwiki__pagetools">
+			    <h3 class="a11y"><?php echo $lang['page_tools']; ?></h3>
+			    <div class="tools">
+				<ul>
+				    <?php echo (new \dokuwiki\Menu\PageMenu())->getListItems(); ?>
+				</ul>
+			    </div>
+			</div>
+		</div><!-- row -->
+	    </div><!-- container -->
+	    <?php include('tpl_footer.php') ?>
+	</div></div><!-- /site -->
 
-            <!-- PAGE ACTIONS -->
-            <div id="dokuwiki__pagetools">
-                <h3 class="a11y"><?php echo $lang['page_tools']; ?></h3>
-                <div class="tools">
-                    <ul>
-                        <?php echo (new \dokuwiki\Menu\PageMenu())->getListItems(); ?>
-                    </ul>
-                </div>
-            </div>
-        </div><!-- /wrapper -->
+	<div class="no"><?php tpl_indexerWebBug() /* provide DokuWiki housekeeping, required in all templates */ ?></div>
+	<div id="screen__mode" class="no"></div><?php /* helper to detect CSS media query in script.js */ ?>
 
-        <?php include('tpl_footer.php') ?>
-    </div></div><!-- /site -->
+	<!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> -->
+	<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script> -->
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
-    <div class="no"><?php tpl_indexerWebBug() /* provide DokuWiki housekeeping, required in all templates */ ?></div>
-    <div id="screen__mode" class="no"></div><?php /* helper to detect CSS media query in script.js */ ?>
-
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
-</body>
+    </body>
 </html>
--- tpl_header.php
+++ tpl_header.php
@@ -8,51 +8,127 @@ if (!defined('DOKU_INC')) die();
 ?>
 
 <!-- ********** HEADER ********** -->
-<div id="dokuwiki__header"><div class="pad group">
+<?php tpl_includeFile('header.html') ?>
 
-    <?php tpl_includeFile('header.html') ?>
+<!-- ナビゲーションバー -->
+<nav class="navbar navbar-expand-md navbar-light">
 
-    <div class="headings group">
-        <ul class="a11y skip">
-            <li><a href="#dokuwiki__content"><?php echo $lang['skip_to_content']; ?></a></li>
-        </ul>
+    <!-- サブコンポーネント -->
+    <div class="container">
 
-        <h1 style="font-weight: 900; "><?php echo $conf['title'] ?></h1>
-        <?php if ($conf['tagline']): ?>
-            <p class="claim"><?php echo $conf['tagline']; ?></p>
-        <?php endif ?>
+	<!-- ブランドのhref ..ルートの指定方法がわからないのでphpを使う意味がない状態になっている。-->
+	<?php
+	echo '<a class="navbar-brand" style="font-weight:900;" href="'
+	    .'/doku.php?id=start">'
+            .$conf['title'].'</a>';
+	?>
+
+	<!-- タグライン？ -->
+	<?php if ($conf['tagline']): ?>
+	    <p class="claim"><?php echo $conf['tagline']; ?></p>
+	<?php endif ?>
+
+	<!-- ハンバーガーボタン -->
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-content" aria-controls="navbar-content" aria-expanded="false" aria-label="Toggle navigation">
+	    <span class="navbar-toggler-icon"></span>
+	</button>
+
+	<!-- <ul class="a11y skip">
+	     <li><a href="#dokuwiki__content"><?php echo $lang['skip_to_content']; ?></a></li>
+	     </ul> -->
+
+	<!-- ナビゲーション -->
+	<div class="collapse navbar-collapse" id="navbar-content">
+
+	    <ul class="navbar-nav mr-auto">
+	    </ul>
+
+	    <!-- トップメニュー -->
+	    <ul class="navbar-nav py-3">
+		<!-- USER TOOLS -->
+
+		<!-- ドロップダウン(ユーザメニュー) -->
+		<li class="nav-item mx-2 dropdown">
+		    <a href="#" class="nav-link dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			<?php if ($conf['useacl']): ?>
+			    <?php
+			    if (!empty($_SERVER['REMOTE_USER'])) {
+				echo $_SERVER['REMOTE_USER'];
+			    }else{
+				echo 'N/A';
+			    }
+			    ?>
+		    </a>
+		    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+			<?php
+			$items = (new \dokuwiki\Menu\UserMenu())->getItems();
+			foreach($items as $item) {
+			    echo '<li><a class="nav-item mx-2" href="'.$item->getLink().'" title="'.$item->getTitle().'">'
+			       . $item->getLabel()
+			       . '</a></li>';
+			}
+			?>
+		    </ul>
+		</li>
+			<?php endif ?>
+
+			<!-- ページメニュー -->
+			<li class="nav-item mx-2 dropdown d-lg-none">
+			    <a href="#" class="nav-link dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">Page</a>
+			    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+				<?php
+				$items = (new \dokuwiki\Menu\PageMenu())->getItems();
+				foreach($items as $item) {
+				    echo '<li><a class="nav-item mx-2" href="'.$item->getLink().'" title="'.$item->getTitle().'">'
+				       . $item->getLabel()
+				       . '</a></li>';
+				}
+				?>
+			    </ul>
+			</li>
+
+			<!-- モバイルツール -->
+			<li class="nav-item mx-2 dropdown">
+			    <a href="#" class="nav-link dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">Mobile</a>
+			    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+				<?php
+				$items = (new \dokuwiki\Menu\MobileMenu())->getItems();
+				foreach($items as $item) {
+				    echo '<li><a class="nav-item mx-2" href="'.$item->getLink().'" title="'.$item->getTitle().'">'
+				       . $item->getLabel()
+				       . '</a></li>';
+				}
+				?>
+			    </ul>
+			</li>
+
+			<!-- 検索 -->
+			<li class="nav-item mx-2">
+			    <?php /*tpl_searchform();*/ ?>
+			    <form action="/doku.php?id=start" method="get" role="search" class="search doku_form" id="dw__search" accept-charset="utf-8"><input type="hidden" name="do" value="search" />
+				<input type="hidden" name="id" value="start" /><div class="no">
+				    <input name="q" type="text" class="edit" title="[F]" accesskey="f" placeholder="" autocomplete="on" id="qsearch__in" value="" />
+				    <button value="1" type="submit" title="検索" class="btn">検索</button>
+				</div>
+	</div>
+			    </form>
+			</li>
+
+			<!-- BREADCRUMBS -->
+			<?php if($conf['breadcrumbs'] || $conf['youarehere']): ?>
+			    <!-- <div class="breadcrumbs">
+				 <?php if($conf['youarehere']): ?>
+				 <div class="youarehere"><?php tpl_youarehere() ?></div>
+				 <?php endif ?>
+				 <?php if($conf['breadcrumbs']): ?>
+				 <div class="trace"><?php tpl_breadcrumbs() ?></div>
+				 <?php endif ?>
+				 </div> -->
+			<?php endif ?>
+
+			<hr class="a11y" />
+	    </ul>
     </div>
-
-    <div class="tools group">
-        <!-- USER TOOLS -->
-        <?php if ($conf['useacl']): ?>
-            <div id="dokuwiki__usertools">
-                <h3 class="a11y"><?php echo $lang['user_tools']; ?></h3>
-                <ul>
-                    <?php
-                        if (!empty($_SERVER['REMOTE_USER'])) {
-                            echo '<li class="user">';
-                            tpl_userinfo(); /* 'Logged in as ...' */
-                            echo '</li>';
-                        }
-                        echo (new \dokuwiki\Menu\UserMenu())->getListItems('action ');
-                    ?>
-                </ul>
-            </div>
-        <?php endif ?>
-
-        <!-- SITE TOOLS -->
-        <div id="dokuwiki__sitetools">
-            <h3 class="a11y"><?php echo $lang['site_tools']; ?></h3>
-            <?php tpl_searchform(); ?>
-            <div class="mobileTools">
-                <?php echo (new \dokuwiki\Menu\MobileMenu())->getDropdown($lang['tools']); ?>
-            </div>
-            <ul>
-                <?php echo (new \dokuwiki\Menu\SiteMenu())->getListItems('action ', false); ?>
-            </ul>
-        </div>
-
     </div>
-    <hr class="a11y" />
-</div></div><!-- /header -->
+</nav>
+<!-- /header -->
