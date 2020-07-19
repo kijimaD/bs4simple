bf49702cb9cf89c18b4b7e24b4ced56d3b25ba3f
kijima <norimaking777@gmail.com>
2020-07-19 18:12:46 +0900

    Fix class problem

--- main.php
+++ main.php
@@ -45,8 +45,8 @@ function ua_smt (){
 	<?php if($ID == 'start'): ?>
 	    <div id="dokuwiki__site"><div id="dokuwiki__top" class="site <?php echo tpl_classes(); ?> <?php
 												      echo ($showSidebar) ? 'showSidebar' : ''; ?> <?php echo ($hasSidebar) ? 'hasSidebar' : ''; ?>">
-		<?php else: ?>
-		<div id="dokuwiki__site"><div id="dokuwiki__top" class="site">
+	<?php else: ?>
+		<div id="dokuwiki__site"><div id="dokuwiki__top" class="site <?php echo tpl_classes(); ?>">
 	<?php endif; ?><!-- /start -->
 
         <?php include('tpl_header.php') ?>
@@ -117,16 +117,16 @@ function ua_smt (){
 		</div>
 	    </div><!-- row -->
 	</div><!-- container -->
-	    </div><!-- wrapper -->
-	    <?php include('tpl_footer.php') ?>
-	    </div></div><!-- /site -->
+		</div><!-- wrapper -->
+		<?php include('tpl_footer.php') ?>
+		</div></div><!-- /site -->
 
-	    <div class="no"><?php tpl_indexerWebBug() /* provide DokuWiki housekeeping, required in all templates */ ?></div>
-	    <div id="screen__mode" class="no"></div><?php /* helper to detect CSS media query in script.js */ ?>
+		<div class="no"><?php tpl_indexerWebBug() /* provide DokuWiki housekeeping, required in all templates */ ?></div>
+		<div id="screen__mode" class="no"></div><?php /* helper to detect CSS media query in script.js */ ?>
 
-	    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-	    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script> -->
-	    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script> -->
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
     </body>
 </html>
