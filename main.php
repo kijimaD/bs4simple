6fdb6ee4357c5cbd9d0576942d1cd6d3d2352bd7
kijima <norimaking777@gmail.com>
2020-07-19 18:38:08 +0900

    Fix sidebar display in mobile

--- main.php
+++ main.php
@@ -53,15 +53,9 @@ function ua_smt (){
 	<div class="wrapper group">
 	    <div class="container">
 		<div class="row">
+		    <div class="col-12 px-4 mt-4">';
 
-		    <?php if (ua_smt() == true){
-			echo '<div class="col-12 mt-4">';
-		    } else {
-			echo '<div class="col-12 px-4 mt-4">';
-		    } ?>
-
-		    <?php if($ID == 'start'): ?>
-			<?php if(ua_smt() == false): ?>
+			<?php if($ID == 'start'): ?>
 			    <?php if($showSidebar): ?>
 				<!-- ********** ASIDE ********** -->
 				<div id="dokuwiki__aside"><div class="pad aside include group border-right border-dark">
@@ -74,51 +68,50 @@ function ua_smt (){
 				    </div></div>
 				</div></div><!-- /aside -->
 			    <?php endif; ?>
-			<?php endif; ?>
-		    <?php endif; ?><!-- /start -->
+			<?php endif; ?><!-- /start -->
 
 
-		    <!-- ********** CONTENT ********** -->
+			<!-- ********** CONTENT ********** -->
 
-		    <div id="dokuwiki__content"><div class="pad group">
-			<?php html_msgarea() ?>
+			<div id="dokuwiki__content"><div class="pad group">
+			    <?php html_msgarea() ?>
 
-			<div class="page group p-0">
+			    <div class="page group p-0">
+				<?php tpl_flush() ?>
+				<?php tpl_includeFile('pageheader.html') ?>
+				<!-- wikipage start -->
+				<?php if (ua_smt() == true){
+				    tpl_content(false); /* if mobile, not display TOC. */
+				} else {
+				    tpl_content();
+				}
+				?>
+				<!-- wikipage stop -->
+				<?php tpl_includeFile('pagefooter.html') ?>
+
+				<small><div class="docInfo text-right text-secondary"><?php tpl_pageinfo() ?></div></small>
+			    </div>
+
+
+
 			    <?php tpl_flush() ?>
-			    <?php tpl_includeFile('pageheader.html') ?>
-			    <!-- wikipage start -->
-			    <?php if (ua_smt() == true){
-				tpl_content(false); /* if mobile, not display TOC. */
-			    } else {
-				tpl_content();
-			    }
-			    ?>
-			    <!-- wikipage stop -->
-			    <?php tpl_includeFile('pagefooter.html') ?>
-
-			    <small><div class="docInfo text-right text-secondary"><?php tpl_pageinfo() ?></div></small>
-			</div>
-
-
-
-			<?php tpl_flush() ?>
-		    </div></div>
-		</div>
-		<!-- /content -->
-
-		<!-- PAGE ACTIONS -->
-		<div id="dokuwiki__pagetools">
-		    <h3 class="a11y"><?php echo $lang['page_tools']; ?></h3>
-		    <div class="tools">
-			<ul>
-			    <?php echo (new \dokuwiki\Menu\PageMenu())->getListItems(); ?>
-			</ul>
+			</div></div>
 		    </div>
-		</div>
-	    </div><!-- row -->
-	</div><!-- container -->
-		</div><!-- wrapper -->
-		<?php include('tpl_footer.php') ?>
+		    <!-- /content -->
+
+		    <!-- PAGE ACTIONS -->
+		    <div id="dokuwiki__pagetools">
+			<h3 class="a11y"><?php echo $lang['page_tools']; ?></h3>
+			<div class="tools">
+			    <ul>
+				<?php echo (new \dokuwiki\Menu\PageMenu())->getListItems(); ?>
+			    </ul>
+			</div>
+		    </div>
+		</div><!-- row -->
+	    </div><!-- container -->
+	</div><!-- wrapper -->
+	<?php include('tpl_footer.php') ?>
 		</div></div><!-- /site -->
 
 		<div class="no"><?php tpl_indexerWebBug() /* provide DokuWiki housekeeping, required in all templates */ ?></div>
